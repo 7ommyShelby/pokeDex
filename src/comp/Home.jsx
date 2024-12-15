e854183f85454fa1686c41759ff9a0bf1ba475c8
7ommyShelby <aryaalsaher@gmail.com>
2024-12-15 15:38:11 +0530

    update

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -1,14 +1,16 @@
-import React, { useState, Suspense, useEffect } from 'react'
+import React, { useState, Suspense, useEffect, useCallback } from 'react'
 import axios from 'axios'
+import Info from './Info';
 
 const Home = () => {
 
     const [pokemon, setPokemon] = useState('')
     const [loading, setLoading] = useState(false);
     const [searchResult, setSearchResult] = useState(null)
+    const [types, setTypes] = useState(null)
+    const [filter, setFilter] = useState(null)
 
-
-    console.log(loading, "loading");
+    // console.log(loading, "loading");
 
     const search = () => {
 
@@ -16,26 +18,59 @@ const Home = () => {
 
         setLoading(true);
 
-        try {
-            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
-                .then((response) => {
-                    console.log(response);
-                    setPokemon("");
-                    setSearchResult(response.data);
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-                .finally(() => {
-                    setLoading(false)
-                })
-        } catch (error) {
-            console.log(error);
-        }
-
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+            .then((response) => {
+                console.log(response);
+                setPokemon("");
+                setSearchResult(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false)
+            })
     }
 
+    const alltypes = useCallback(() => {
+
+        axios.get('https://pokeapi.co/api/v2/type')
+            .then((response) => {
+                setTypes(response.data.results)
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [types])
+
+    const filterRes = (type) => {
+        setFilter(null)
+        if (type === "") {
+            return;
+        }
+
+        const url = types.find((e) => e.name === type).url
+
+        axios.get(url)
+            .then((response) => {
+                setFilter(response.data.pokemon)
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+
+    useEffect(() => {
+        alltypes()
+    }, [])
+
+
     console.log(pokemon);
+    console.log(types);
+    console.log(filter);
 
 
     return (
@@ -45,13 +80,33 @@ const Home = () => {
                     <h1 className='font-bold text-xl'>PokeDEX</h1>
                 </nav>
 
-                <div className='p-2 bg-red-200 flex items-center justify-center gap-2 m-auto flex-wrap'>
-                    <label className='mr-2' htmlFor="">Find Pokemon</label>
-                    <input value={pokemon} onChange={(e) => { setPokemon(e.target.value) }} className='rounded-md p-1 mr-2 placeholder-pink-500' placeholder='Search...' type="text" name="" id="" />
-                    <button onClick={search} className='shadow px-2 rounded-md bg-red-400'>Search</button>
+                <div className='hero p-2 h-full flex-col flex items-center justify-center gap-2 m-auto flex-wrap'>
+
+                    <div>
+                        <label htmlFor="">Filter</label>
+                        <select onChange={(e) => { filterRes(e.target.value) }} name="" id="">
+                            <option value="">Select a type</option>
+                            {
+                                types?.map((e) => {
+                                    return (
+                                        <>
+                                            <option value={e.name}>{e.name}</option>
+                                        </>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
+
+                    <div className=''>
+                        <label className='mr-2' htmlFor="">Find Pokemon</label>
+                        <input value={pokemon} onChange={(e) => { setPokemon(e.target.value) }} className='rounded-md p-1 mr-2 placeholder-pink-500' placeholder='Search...' type="text" name="" id="" />
+                        <button onClick={search} className='shadow px-2 rounded-md bg-red-400'>Search</button>
+                    </div>
+
                 </div>
 
-                <div className='w-full text-center h-full'>
+                <div className='w-full text-center'>
                     {
                         loading ? (
                             <>
@@ -63,15 +118,35 @@ const Home = () => {
                             <div className='h-full w-full flex items-center justify-center'>
                                 <Card data={searchResult} />
                             </div>
-                        ) : (
-                            <h3 className="font-semibold mt-10">Look for your pokemon...</h3>
-                        )
+                        ) : filter ? (
+                            <>
+                                <div className='flex gap-2 w-full flex-wrap'>
+                                    {
+                                        filter.map((e) => {
+                                            return (
+                                                <>
+
+                                                    <Info url={e.pokemon.url} />
+
+                                                </>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </>
+                        ) : null
                     }
                 </div>
+
+
             </main>
         </>
     )
 }
+
+// for loading
+/* <h3 className="font-semibold mt-10">Look for your pokemon...</h3> */
+
 
 const Loader = () => {
     return (
